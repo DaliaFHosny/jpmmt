0a790be71f6b88212d22beb6d65ca0b96bdd160e
daklinque@googlemail.com <daklinque@googlemail.com@a77daa10-1fe1-2050-ab87-0f19de8b04b3>
2013-09-25 20:15:17 +0000

    Basic structure for the evaluation added.

---
+++ trunk/src/main/java/de/lsem/evaluation/onetoone/ModelAlignment.java
@@ -0,0 +1,50 @@
+package de.lsem.evaluation.onetoone;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Copyright (c) 2013 Christopher Klinkmüller
+ * 
+ * This software is released under the terms of the
+ * MIT license. See http://opensource.org/licenses/MIT
+ * for more information.
+ */
+
+public class ModelAlignment {
+	private String firstModel;
+	private String secondModel;
+	private Set<ActivityMatch> activityMatches;
+	
+	public ModelAlignment(String firstModel, String secondModel) {
+		this.activityMatches = new HashSet<ActivityMatch>();
+		this.firstModel = firstModel;
+		this.secondModel = secondModel;
+	}
+	
+	public String getFirstModel() {
+		return firstModel;
+	}
+	
+	public String getSecondModel() {
+		return secondModel;
+	}
+	
+	public void addActivityMatch(ActivityMatch match) {
+		this.activityMatches.add(match);
+	}
+	
+	public void addActivityMatches(Collection<ActivityMatch> matches) {
+		this.activityMatches.addAll(activityMatches);
+	}
+	
+	public Set<ActivityMatch> getActivityMatches() {
+		return Collections.unmodifiableSet(this.activityMatches);
+	}
+	
+	public void removeActivityMatch(ActivityMatch match) {
+		this.activityMatches.remove(match);
+	}
+}
---
+++ trunk/src/main/java/de/lsem/evaluation/onetoone/ModelAlignmentReader.java
@@ -0,0 +1,11 @@
+package de.lsem.evaluation.onetoone;
+
+public class ModelAlignmentReader {
+	public ModelAlignmentReader() {
+		
+	}
+	
+	public ModelAlignment read(String filename) {
+		return null;
+	}
+}
