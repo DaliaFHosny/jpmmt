06c97c30e70569c462b5861ec27bbedd87d70efe
daklinque <daklinque@googlemail.com>
2013-09-26 17:52:08 +0000

    Evaluation based on One To One Matches updated.

---
+++ src/main/java/de/lsem/evaluation/EvaluationMeasures.java
@@ -0,0 +1,124 @@
+package de.lsem.evaluation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EvaluationMeasures {
+	private double stdPrecision;
+	private double stdRecall;
+	private double stdFmeasure;
+	private double meanPrecision;
+	private double meanRecall;
+	private double meanFmeasure;
+	private List<Integer> tps;
+	private List<Integer> fps;
+	private List<Integer> fns;
+	
+	public EvaluationMeasures() {
+		this.fns = new ArrayList<Integer>();
+		this.fps = new ArrayList<Integer>();
+		this.tps = new ArrayList<Integer>();
+	}
+	
+	public double getStdPrecision() {
+		return stdPrecision;
+	}
+	
+	public void setStdPrecision(double stdPrecision) {
+		this.stdPrecision = stdPrecision;
+	}
+	
+	public double getStdRecall() {
+		return stdRecall;
+	}
+	
+	public void setStdRecall(double stdRecall) {
+		this.stdRecall = stdRecall;
+	}
+	
+	public double getStdFmeasure() {
+		return stdFmeasure;
+	}
+	
+	public void setStdFmeasure(double stdFmeasure) {
+		this.stdFmeasure = stdFmeasure;
+	}
+	
+	public double getMeanPrecision() {
+		return meanPrecision;
+	}
+	
+	public void setMeanPrecision(double meanPrecision) {
+		this.meanPrecision = meanPrecision;
+	}
+	
+	public double getMeanRecall() {
+		return meanRecall;
+	}
+	
+	public void setMeanRecall(double meanRecall) {
+		this.meanRecall = meanRecall;
+	}
+	
+	public double getMeanFmeasure() {
+		return meanFmeasure;
+	}
+	
+	public void setMeanFmeasure(double meanFmeasure) {
+		this.meanFmeasure = meanFmeasure;
+	}
+
+	public void add(int tp, int fp, int fn) {
+		this.tps.add(tp);
+		this.fps.add(fp);
+		this.fns.add(fn);
+	}
+
+	public void calculate() {
+		List<Double> precisions = new ArrayList<Double>();
+		List<Double> recalls = new ArrayList<Double>();
+		List<Double> fmeasures = new ArrayList<Double>();
+		
+		for (int a = 0; a < this.tps.size(); a++) {
+			double tp = this.tps.get(a);
+			double fp = this.fps.get(a);
+			double fn = this.fns.get(a);
+			double precision = tp / (tp + fp);
+			double recall = tp / (tp + fn);
+			double fmeasure = 2 * precision * recall / (precision * recall);
+			precisions.add(precision);
+			recalls.add(recall);
+			fmeasures.add(fmeasure);
+		}
+		
+		this.meanPrecision = this.calculateMean(precisions);
+		this.meanRecall = this.calculateMean(recalls);
+		this.meanFmeasure = this.calculateMean(fmeasures);
+		
+		this.stdPrecision = this.calculateStd(precisions, this.getMeanPrecision());
+		this.stdRecall = this.calculateStd(recalls, this.getMeanRecall());
+		this.stdFmeasure = this.calculateStd(fmeasures, this.getMeanFmeasure());
+	}
+
+	private double calculateStd(List<Double> values, double mean) {
+		double std = 0;
+		
+		for (Double value : values) {
+			std += Math.pow(value - mean, 2);
+		}
+		std = Math.sqrt(std / values.size());
+		
+		return std;
+	}
+
+	private double calculateMean(List<Double> values) {
+		double mean = 0;
+		
+		for (Double val : values) {
+			mean += val;
+		}
+		
+		return mean / values.size();
+	}	
+	
+}
--- src/main/java/de/lsem/evaluation/onetoone/ModelAlignmentEvaluation.java
+++ src/main/java/de/lsem/evaluation/onetoone/ModelAlignmentEvaluation.java
@@ -1,6 +1,14 @@
 package de.lsem.evaluation.onetoone;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+
+import de.lsem.evaluation.EvaluationMeasures;
+import de.lsem.process.matching.FragmentMatch;
+import de.lsem.process.matching.ProcessMapping;
+import de.lsem.process.model.ProcessNode;
 
 /*
  * Copyright (c) 2013 Christopher Klinkmüller
@@ -20,5 +28,66 @@ class ModelAlignmentEvaluation {
 	
 	public ModelAlignmentEvaluation(Map<String, Map<String, ModelAlignment>> standard) {
 		this.standard = standard;	
-	}	
+	}
+	
+	public EvaluationMeasures evaluate(Collection<ProcessMapping> mappings) {
+		List<ModelAlignment> alignments = this.transform(mappings);
+		EvaluationMeasures metrics = this.evaluateAlignments(alignments);	
+		return metrics;
+	}
+
+	private EvaluationMeasures evaluateAlignments(Collection<ModelAlignment> alignments) {
+		EvaluationMeasures measures = new EvaluationMeasures();
+		
+		for (ModelAlignment determinedAlignment : alignments) {
+			ModelAlignment standardAlignment = this.standard.get(determinedAlignment.getFirstModel()).get(determinedAlignment.getSecondModel());
+			this.evaluateAlignment(determinedAlignment, standardAlignment, measures);
+		}
+		
+		measures.calculate();
+		return measures;
+	}
+
+	private void evaluateAlignment(ModelAlignment determinedAlignment, ModelAlignment standardAlignment, EvaluationMeasures measures) {
+		int tp = 0;
+		int fp = 0;
+		int fn = 0;
+		
+		for (ActivityMatch deterMatch : determinedAlignment.getActivityMatches()) {
+			for (ActivityMatch standMatch : standardAlignment.getActivityMatches()) {
+				if (deterMatch.getFirstLabel().equals(standMatch.getFirstLabel()) && standMatch.getSecondLabel().equals(deterMatch.getSecondLabel())) {
+					tp += 1;
+					break;
+				}
+			}
+		}
+		
+		fp = determinedAlignment.getActivityMatches().size() - tp;
+		fn = standardAlignment.getActivityMatches().size() - tp;	
+		measures.add(tp, fp, fn);
+	}
+
+	private List<ModelAlignment> transform(Collection<ProcessMapping> mappings) {
+		List<ModelAlignment> alignments = new ArrayList<ModelAlignment>();
+		
+		for (ProcessMapping mapping : mappings) {
+			alignments.add(this.transform(mapping));
+		}
+		
+		return alignments;
+	}
+
+	private ModelAlignment transform(ProcessMapping mapping) {
+		ModelAlignment alignment = new ModelAlignment(mapping.getModel1().getName(), mapping.getModel2().getName());
+		
+		for (FragmentMatch match : mapping.getFragmentMatch()) {
+			for (ProcessNode node1 : match.getFragment1().getProcessNodes()) {
+				for (ProcessNode node2 : match.getFragment2().getProcessNodes()) {
+					alignment.addActivityMatch(new ActivityMatch(node1.getLabel(), node2.getLabel()));
+				}
+			}
+		}
+		
+		return alignment;
+	}
 }
